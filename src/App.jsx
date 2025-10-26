0f0dd1856300e8d784ca5e0eb932d0aa52a98be2
Sushilkushwah14 <sushilkushwah1411@gmail.com>
2025-10-26 17:18:59 +0530

    setup completed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className=''>
+    <div className='text-red-300 text-7xl'>
+      hellommmmm
+    </div>
+      
+    </div>
+  )
+}
+
+export default App
