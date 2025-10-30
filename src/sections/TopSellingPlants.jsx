48abc00f4da4155e9222f0c6ace081252a6937fb
Sushilkushwah14 <sushilkushwah1411@gmail.com>
2025-10-30 12:01:35 +0530

    adding the Section title after suttle modification in it

--- src/sections/TopSellingPlants.jsx
+++ src/sections/TopSellingPlants.jsx
@@ -1,25 +1,7 @@
-// import React from "react";
-// import SectionTitle from "../components/SectionTitle";
-// import PlantCard from "../components/PlantCard";
-// import { topSellingPlants } from "../data/topSellingPlants";
 
-// const TopSellingPlants = () => {
-//   return (
-//     <section className="py-12 container mx-auto">
-//       <SectionTitle title="Our Top Selling Plants" />
-
-//       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6 mt-8">
-//         {topSellingPlants.map((plant) => (
-//           <PlantCard key={plant.id} data={plant} />
-//         ))}
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default TopSellingPlants;
 import { topSellingPlants } from "../data/topSellingPlants";
 import PlantCard from "../components/PlantCard";
+import TopSellingCard from "../components/TopSellingCards";
 import SectionTitle from "../components/SectionTitle";
 
 const TopSellingPlants = () => {
@@ -29,9 +11,16 @@ const TopSellingPlants = () => {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 mt-8">
         {topSellingPlants.map((plant) => (
-          <PlantCard key={plant.id} data={plant} />
+          <TopSellingCard
+          key={plant.id}
+          image={`/${plant.image}`}
+          name={plant.name}
+          about={plant.about}
+          price={plant.price}
+        />
         ))}
       </div>
     </section>
   );
 };
+export default TopSellingPlants;
