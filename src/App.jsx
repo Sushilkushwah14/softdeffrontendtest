7b210c0c8aa2ca77ebb205e0ca80044b7eae29b0
Sushilkushwah14 <sushilkushwah1411@gmail.com>
2025-10-30 12:12:36 +0530

    importing the trending plants,topselling plants and customer reviews sections and updating the css ,along with app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 
 
+import TopSellingCard from "./components/TopSellingCards";
+
 import Home from "./pages/Home";
+import TopSellingPlants from "./sections/TopSellingPlants";
+import TrendingPlants from "./sections/TrendingPlants";
 
 
 const  App=()=> {
   return (
    <>
       <Home/>
-    </>
+   </>
   );
 }
 export default App
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,57 @@
-import React from 'react'
-import HeroBanner from '../components/HeroBanner'
-import Navbar from '../components/Navbar'
+
+import HeroBanner from "../components/HeroBanner";
+import Navbar from "../components/Navbar";
+import TopSellingPlants from "../sections/TopSellingPlants";
+import TrendingPlants from "../sections/TrendingPlants";
+import CustomerReviews from "../sections/CustomerReview";
 
 const Home = () => {
   return (
-    <div>
-    <Navbar/>
-      <HeroBanner/>
-    </div>
-  )
-}
+    <div className="relative w-full min-h-screen bg-black text-white overflow-x-hidden overflow-y-auto">
+      <Navbar />
 
-export default Home
+      <section
+        className="relative w-full min-h-screen bg-cover bg-center"
+        style={{
+          backgroundImage: "url('/herobanner.jpg')",
+          backgroundPositionY: "-125px",
+        }}
+      >
+        <div className="absolute inset-0 bg-black/40 backdrop-blur-xs"></div>
+
+        <div className="relative z-10 flex flex-col items-start justify-center px-[60px] pt-[250px] pb-[100px]">
+          <HeroBanner />
+        </div>
+
+        <div className="absolute z-10 bottom-[-220px] w-full flex flex-col items-center">
+          <h2
+            className="text-white font-semibold text-[54px] mb-[60px]"
+            style={{
+              width: "492.5px",
+              height: "67px",
+              borderRadius: "20px",
+              opacity: 1,
+            }}
+          >
+            Our Trendy Plants
+          </h2>
+
+          <div className="w-full flex justify-center">
+            <TrendingPlants />
+          </div>
+        </div>
+      </section>
+
+      <section className="relative bg-[#0E1C0E] text-white pt-[400px] pb-[150px]">
+        <TopSellingPlants />
+      </section>
+
+
+      <section className="relative bg-[#0E1C0E] text-white py-[150px]">
+        <CustomerReviews />
+      </section>
+    </div>
+  );
+};
+
+export default Home;
