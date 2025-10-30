3b8014208f857ad41b77edc0a46493cc5f3c4ccd
Sushilkushwah14 <sushilkushwah1411@gmail.com>
2025-10-30 12:10:31 +0530

    created the array of data of review sections

---
+++ src/data/reviewData.jsx
@@ -0,0 +1,30 @@
+export const reviewData = [
+  {
+    photo: "/P1.png",
+    name: "Shelly Russel",
+    review:
+      "Just got my hands on some absolutely awesome plants, and I couldn’t be happier!",
+    starImage: "/star.png",
+  },
+  {
+    photo: "/P2.png",
+    name: "Kevin Thomas",
+    review:
+      "These plants are so fresh and healthy! The packaging was perfect too.",
+    starImage: "/star.png",
+  },
+  {
+    photo: "/P3.png",
+    name: "Sophia White",
+    review:
+      "Beautiful plants and quick delivery! My living room looks so lively now.",
+    starImage: "/star.png",
+  },
+  {
+    photo: "/P4.png",
+    name: "Ronnie Hamill",
+    review:
+      "I can't express how thrilled I am with my new natural plants! They bring such a fresh and vibrant energy to my home.",
+    starImage: "/star.png",
+  },
+];
