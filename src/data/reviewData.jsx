8ef590c501fed5994c330948b2389f885df25940
Sushilkushwah14 <sushilkushwah1411@gmail.com>
2025-10-31 14:48:21 +0530

    final commit

--- src/data/reviewData.jsx
+++ src/data/reviewData.jsx
@@ -1,27 +1,27 @@
 export const reviewData = [
   {
-    photo: "/P1.png",
+    photo: "/im1.jpg",
     name: "Shelly Russel",
     review:
       "Just got my hands on some absolutely awesome plants, and I couldn’t be happier!",
     starImage: "/star.png",
   },
   {
-    photo: "/P2.png",
+    photo: "/im2.jpg",
     name: "Kevin Thomas",
     review:
       "These plants are so fresh and healthy! The packaging was perfect too.",
     starImage: "/star.png",
   },
   {
-    photo: "/P3.png",
+    photo: "/im3.jpg",
     name: "Sophia White",
     review:
       "Beautiful plants and quick delivery! My living room looks so lively now.",
     starImage: "/star.png",
   },
   {
-    photo: "/P4.png",
+    photo: "/im1.jpg",
     name: "Ronnie Hamill",
     review:
       "I can't express how thrilled I am with my new natural plants! They bring such a fresh and vibrant energy to my home.",
